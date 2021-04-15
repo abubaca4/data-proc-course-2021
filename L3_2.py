d291265a7317dcc025e75300518ddc4b55f02e7f
abubaca4 <abubaca4@gmail.com>
2021-04-15 10:46:37 +0300

    lab 3_2 complete

--- L3_2.py
+++ L3_2.py
@@ -31,3 +31,52 @@ def check_5(row1, row2, alpha):
 
 num_row1 = []
 num_row2 = []
+
+layout = [
+    [sg.Text('N:'), sg.InputText('100', key='N'),
+     sg.Text('N:'), sg.InputText('100', key='N_2')],
+    [sg.Text('μ:'), sg.InputText('0', key='μ'),
+     sg.Text('μ:'), sg.InputText('0', key='μ_2')],
+    [sg.Text('σ²:'), sg.InputText('1', key='σ²')],
+    [sg.Submit("Сгенерировать выборку 1"),
+     sg.Submit("Сгенерировать выборку 2")],
+    [sg.Text('Параметры распределения 1'),
+     sg.Text('Параметры распределения 2')],
+    [sg.Output(size=(48, 10), key='out_0'),
+     sg.Output(size=(48, 10), key='out_1')],
+    [sg.Text('Данные для проверки')],
+    [sg.Text('α:'), sg.InputText('0.05', key='α')],
+    [sg.Submit("Проверить гипотезы")],
+    [sg.Output(size=(48, 10), key='out_2')],
+]
+
+window = sg.Window('Lab 3_2', layout)
+
+while True:
+    event, values = window.read()
+    if event in (None, 'Exit', 'Cancel'):
+        break
+    if event == 'Сгенерировать выборку 1':
+        num_row1 = make_row(int(values["N"]), float(
+            values["μ"]), float(values["σ²"]))
+        window["out_0"].update("\n".join(list(map(str, num_row1[:10]))))
+    if event == 'Сгенерировать выборку 2':
+        num_row2 = make_row(int(values["N_2"]), float(
+            values["μ_2"]), float(values["σ²"]))
+        window["out_1"].update("\n".join(list(map(str, num_row2[:10]))))
+    if event == 'Проверить гипотезы':
+        result = ""
+        if (len(num_row1) < 1 or len(num_row2) < 1):
+            result = 'Пожалуйста сначала сгенерируйте выборки'
+        else:
+            result += 'Гипотеза о равенстве математических ожиданий при неизвестных дисперсиях:' + '\n'
+            temp = check_5(num_row1, num_row2, float(values['α']))
+            result += temp[1] + '\n'
+            if temp[0]:
+                result += "Выполняется гипотеза H0"
+            else:
+                result += "Выполняется гипотеза H1"
+        window["out_2"].update(result)
+
+
+window.close()
